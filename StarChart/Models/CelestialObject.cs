b88ca23fc30aacab559d45e658df1a9845b50642
Bárdics Zoltán <zoltan.bardics@yahoo.com>
2019-02-20 06:38:20 +0100

    2.1

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -11,7 +11,7 @@ namespace StarChart.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int? OrbetedObjectId { get; set; }
+        public int? OrbitedObject { get; set; }
         [NotMapped]
         public List<CelestialObject> Satellites { get; set; }
         public TimeSpan OrbitalPeriod { get; set; }
